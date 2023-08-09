7e3c6e7b37a028120b441caaf65feb3ca29bbaf7
Stephane ASSOCLE <mensste30@gmail.com>
2023-08-09 01:00:09 +0000

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Module for task 1"""
 import requests
 
+def top_ten(subreddit):
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
 
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    try:
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
-            (by /u/Large_Alternative_30)"
-        }
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
-            return 0
-        results = response.json().get("data")
-        return results.get("subscribers")
-    except (Exception):
-        print('Not Found')
-        return (0)
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
